d81120ba57f8da76d220b40b0ac533c486793f3a
Amit Deshpande <amitdesh2014@gmail.com>
2020-07-01 15:20:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
   {:home => {
     :team_name =>"Brooklyn Nets", 
     :colors:["Black","White"], 
-    :players => [{:player => "Alan Anderson", :number => "0",:shoe => "16",:points =>"22", :rebounds =>"12", :assists =>"12", :steals =>"3", :blocks =>"1", :slam_dunks =>"1"},{:player => "Reggie Evans", :number => "30",:shoe => "14",:points =>"12", :rebounds =>"12", :assists =>"12", :steals =>"12", :blocks =>"12", :slam_dunks =>"7"},{:player => "Brook Lopez", :number => "11",:shoe => "17",:points =>"17", :rebounds =>"19", :assists =>"10", :steals =>"3", :blocks =>"1", :slam_dunks =>"15"},{:player => "Mason Plumlee", :number => "1",:shoe => "19",:points =>"26", :rebounds =>"11", :assists =>"6", :steals =>"3", :blocks =>"8", :slam_dunks =>"5"},{:player => "Jason Terry", :number => "0",:shoe => "16",:points =>"22", :rebounds =>"12", :assists =>"12", :steals =>"3", :blocks =>"1", :slam_dunks =>"1"}
+    :players => [{:player => "Alan Anderson", :number => "0",:shoe => "16",:points =>"22", :rebounds =>"12", :assists =>"12", :steals =>"3", :blocks =>"1", :slam_dunks =>"1"},{:player => "Reggie Evans", :number => "30",:shoe => "14",:points =>"12", :rebounds =>"12", :assists =>"12", :steals =>"12", :blocks =>"12", :slam_dunks =>"7"},{:player => "Brook Lopez", :number => "11",:shoe => "17",:points =>"17", :rebounds =>"19", :assists =>"10", :steals =>"3", :blocks =>"1", :slam_dunks =>"15"},{:player => "Mason Plumlee", :number => "1",:shoe => "19",:points =>"26", :rebounds =>"11", :assists =>"6", :steals =>"3", :blocks =>"8", :slam_dunks =>"5"},{:player => "Jason Terry", :number => "31",:shoe => "15",:points =>"19", :rebounds =>"12", :assists =>"12", :steals =>"3", :blocks =>"1", :slam_dunks =>"1"}
   }
   :away ={
       {:home => {
